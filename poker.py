c79e5d2901b8a51ed573079d01308bbcf4a2274f
Daniel <yarochewsky@icloud.com>
2017-03-21 15:20:14 -0300

    added comments, and fixed spacing.

--- poker.py
+++ poker.py
@@ -21,6 +21,11 @@ def main():
         play()
         end = raw_input("Play again? Y/N ")
 
+# plays one turn of the simulator, giving the user the option to choose whether
+# or not he wants to randomize the choice of the cards to use in the table and
+# players' hands. Then, evaluates the ranks of the different players' hands,
+# through each turn of the game, and decides whose hands is the best, finally
+# printing who is the winning hand.
 def play():
     try:
         players = int(raw_input("Please, enter the number of players"
@@ -39,33 +44,58 @@ def play():
         playersParty = setHands(players, deck)
         board = setBoard(deck)
     print
-    print ("Cards on the table: ")
-    Card.print_pretty_cards(board)
+    printPlayerHands(playersParty)
+    printTableCards(board)
     print
     summary(playersParty, board, evaluator)
 
-
+# prints how to manually input the cards, using the rank + suit notation
 def tutorial():
+    print
+    print
     print ("You will input each card one by one, in the following format: ")
     print("(card rank)(suit). Ranks are 2-10, J, Q, K, A and suits are "
             + " h: hearts" + PRETTY_SUITS["h"]
             + " d: diamonds" + PRETTY_SUITS["d"]
             + " c: clubs" + PRETTY_SUITS["c"]
             + " s: spades" + PRETTY_SUITS["s"])
+    print
     print("E.g. 10 Hearts " + PRETTY_SUITS["h"] + " will be: 10h ")
 
+# prints all of the cards on the table, or board, in a pretty format, showing
+# the suits in colour.
+def printTableCards(board):
+    print ("Cards on the table: ")
+    print
+    Card.print_pretty_cards(board)
+
+# prints each of the players' hands in a pretty format, showing ths suits in
+# colour
+def printPlayerHands(playersParty):
+    i = 0
+    for player in playersParty:
+        print ("Player " + str(i) + "'s hand: ")
+        print
+        Card.print_pretty_cards(player)
+        print
+        i += 1
+
+# chooses the player's hands manually, prompting the user for input one card at
+# a time
 def setHands(players, deck):
     playersParty = []
     tutorial()
     for player in range(0, players):
             playerHand = []
-            first = raw_input("Choose player " + str(player) + " first card: ")
+            print
+            first = raw_input("Choose player " + str(player) + "'s first card: ")
             if first[0] == "1" :
                 newFirst = "T" + first[2]
             else:
                 newFirst = first
             firstCard = Card.new(newFirst)
-            second = raw_input("Choose player " + str(player) + " second card: ")
+            print
+            second = raw_input("Choose player " + str(player) + "'s second card: ")
             if second[0] == "1":
                 newSecond = "T" + second[2]
             else:
@@ -76,7 +106,7 @@ def setHands(players, deck):
             playersParty.append(playerHand)
     return playersParty
 
-
+# chooses the board cards manually, prompting the user for one card at a time
 def setBoard(deck):
     tutorial()
     board = []
@@ -89,15 +119,17 @@ def setBoard(deck):
         board.append(Card.new(newCard))
     return board
 
+# automatically chooses each of the players' hands and returns a list with all
+# of the players' hands.
 def getHands(players, deck):
     playersParty = []
     for player in range(0, players):
         playerHand = deck.draw(NUM_CARDS_HAND)
         playersParty.append(playerHand)
-        print ("Player " + str(player) + " hand: ")
-        Card.print_pretty_cards(playerHand)
     return playersParty
 
+# prints the summary of the poker turns, bearing who is winning on each of these
+# their corresponding ranks, and the final score of the winning hand.
 def summary(players, board, eval):
     eval.hand_summary(board, players)
 
